af730962d161568bd9997e0585bd83901919a94c
simonslavik <simonslavik007@gmail.com>
2025-09-24 14:56:25 +0200

    added controllers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
-import express from 'express';
+import express, { json, urlencoded, Request } from 'express';
+import productsRoutes from './routes/products/index.js';
+import authRoutes from './routes/auth/index.js';
+import ordersRoutes from './routes/orders/index.js';
+import stripeRoutes from './routes/stripe/index.js';
 
-const port = process.env.PORT || 3000;
+import serverless from 'serverless-http';
+
+const port = 3001;
 const app = express();
 
+app.use(urlencoded({ extended: false }));
+app.use(
+  json({
+    verify: (req: Request, res, buf) => {
+      req.rawBody = buf;
+    },
+  })
+);
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.use('/products', productsRoutes);
+app.use('/auth', authRoutes);
+app.use('/orders', ordersRoutes);
+app.use('/stripe', stripeRoutes);
+
+if (process.env.NODE_ENV === 'dev') {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+export const handler = serverless(app);
